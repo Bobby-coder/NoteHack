5901ca05db211465f9a0b5872b255ed3f4e5ec0d
Bobby-coder <bobbysadhwani612@gmail.com>
2023-02-18 00:04:57 +0530

    height bug fixed

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ function App() {
   )
   return (
     <>
-      <div className={theme}>
-        <div className="container">
+      <div className={`notehack-container-wrapper ${theme}`}>
+        <div className="notehack-container">
           <Header 
             theme={theme} 
             setTheme={setTheme} 
